b871726ebae64dd59fd6d24bd5f73b03801004b6
antonypauson <13antonysun@gmail.com>
2025-05-24 18:31:12 +0530

    looping a triangle

--- chapter-2/loopingATrainagle.js
+++ chapter-2/loopingATrainagle.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+let num = 0;
+
+
+for (let i = 0; i < 7; i++) {
+    let combined = ""; 
+    for (let j = 1; j <= i + 1; j++) {
+        combined += '#'; 
+    }
+    console.log(combined);
+}
