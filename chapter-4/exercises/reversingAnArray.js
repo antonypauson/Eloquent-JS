30c8813f665a379b96a07251ff0e46b84208488a
antonypauson <13antonysun@gmail.com>
2025-05-27 16:45:33 +0530

    reversing an array in place

--- chapter-4/exercises/reversingAnArray.js
+++ chapter-4/exercises/reversingAnArray.js
@@ -1,5 +1,24 @@
 let myArray = ["A", "B", "C", "D"]; 
 console.log(reverseArray(myArray));
+console.log(reverseArrayInPlace(myArray));
+console.log(myArray); 
+
+let arrayValues = [1,2,3,4,5]; 
+reverseArrayInPlace(arrayValues); 
+console.log(arrayValues); 
+
+function reverseArrayInPlace(myArray) {
+    let start = 0; 
+    let end = myArray.length - 1; 
+
+    while (start <= end) {
+        let temp = myArray[start];
+        myArray[start] = myArray[end];
+        myArray[end] = temp; 
+        start++; 
+        end--; 
+    }
+}
 
 function reverseArray(myArray) {
     let result = []; 
@@ -7,6 +26,5 @@ function reverseArray(myArray) {
     for (let each of myArray) {
         result.unshift(each); 
     }
-
     return result; 
 }
