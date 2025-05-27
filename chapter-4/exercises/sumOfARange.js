b406de28d5951386a2735affc4673f854bfa3799
antonypauson <13antonysun@gmail.com>
2025-05-27 16:33:13 +0530

    finished sum of a range

--- chapter-4/exercises/sumOfARange.js
+++ chapter-4/exercises/sumOfARange.js
@@ -1,11 +1,20 @@
 console.log(range(1,10)); 
-console.log(sum(range(1,10))); 
+console.log(sum(range(1,10)));
+console.log(range(1,10,2));  
+console.log(range(5,2,-1)); 
 
-function range(start, end) {
+function range(start, end, step=1) {
     let myArr = []; 
-    for (let i = start; i <= end; i++) {
-        myArr.push(i); 
+    if (step < 0) {
+        for (let i = start; i >= end; i += step) {
+            myArr.push(i); 
+        }
+    } else {
+        for (let i = start; i <= end; i += step) {
+            myArr.push(i); 
     }
+    }
+    
     return myArr;
 }
 
