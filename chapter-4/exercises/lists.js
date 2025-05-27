71ebf248202c036d7b27313638f4eb7c7ca7eb60
antonypauson <13antonysun@gmail.com>
2025-05-27 18:02:34 +0530

    nth

--- chapter-4/exercises/lists.js
+++ chapter-4/exercises/lists.js
@@ -13,6 +13,17 @@ let list = {
 console.log(arrayToList([10,20])); 
 console.log(listToArray(arrayToList([10,20,30])));
 console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 1));
+
+function nth(list, index) {
+    let counter = 0;
+    let node = list;  
+    while (counter < index) {
+        node = node.rest; 
+        counter++; 
+    }
+    return node.value;
+}
 
 function prepend(value, list) {
     let newList = {value: value, rest: list};
