bddbebf73712d8cd88a23c82b9c6112b676f8504
antonypauson <13antonysun@gmail.com>
2025-05-28 10:08:49 +0530

    higher order functions

--- chapter-5/higherOrderFunctions.js
+++ chapter-5/higherOrderFunctions.js
@@ -1,1 +1,23 @@
-//begin
+function greaterThan(n) {
+    return m => m > n; 
+} //closure example
+
+let greaterThan10 = greaterThan(10); 
+console.log(greaterThan10(50));
+console.log("----------");
+
+
+function noisy(f) {
+    return(...args) => {
+        console.log("calling with", args);
+        let result = f(...args); 
+        console.log("called with", args, ", returned", result);
+        return result;  
+    }
+}
+noisy(Math.min)(3,2,1);
+//noisy(Math.min) first returns a function
+//then (3,2,1) is called
+
+console.log("----------");
+
