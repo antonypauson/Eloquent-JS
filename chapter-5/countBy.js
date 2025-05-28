acf0dadaf5b7bdf9a723a84fb58dbd4990cb8148
antonypauson <13antonysun@gmail.com>
2025-05-28 12:47:26 +0530

    find()

---
+++ chapter-5/countBy.js
@@ -0,0 +1,16 @@
+console.log(countBy([1,2,3,4,5], n => n > 2));
+
+function countBy(items, groupName) {
+    let counts = [];
+    for (let each of items) {
+        let name = groupName(each);
+        let known = counts.find(s => s.name == name);
+        
+        if (!known) {
+            counts.push({name: name, count: 1}); 
+        } else {
+            known.count++; 
+        }
+    } 
+    return counts; 
+}
