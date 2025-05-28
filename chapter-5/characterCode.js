52ec4a7bbd4beaaa9af4d54d34e2f7c96810e37b
antonypauson <13antonysun@gmail.com>
2025-05-28 12:30:30 +0530

    utf-16 issues

---
+++ chapter-5/characterCode.js
@@ -0,0 +1,7 @@
+let horseShoe = "🐴👟";
+console.log(horseShoe.length);
+//console.log(horseShoe[2]);
+
+for (let each of horseShoe) {
+    console.log(each); 
+}
--- chapter-5/characterScript.js
+++ chapter-5/characterScript.js
@@ -1,6 +1,7 @@
 const SCRIPTS = require('./SCRIPTS.js'); 
 
 console.log(characterScript(121)); //find script with range contains 121
+console.log(characterScript2(121)); //find script with range contains 121
 
 function characterScript(code) {
     for (let script of SCRIPTS) {
@@ -10,3 +11,5 @@ function characterScript(code) {
     }
     return null; 
 }
+
+
