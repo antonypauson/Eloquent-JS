f159ce97445046915f2e749fb7c58776245a6265
antonypauson <13antonysun@gmail.com>
2025-05-28 12:18:37 +0530

    some()

---
+++ chapter-5/characterScript.js
@@ -0,0 +1,12 @@
+const SCRIPTS = require('./SCRIPTS.js'); 
+
+console.log(characterScript(121)); //find script with range contains 121
+
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => { 
+            return code >= from && code < to; //code in that range -> true
+        })) return script;                    //then return that script
+    }
+    return null; 
+}
