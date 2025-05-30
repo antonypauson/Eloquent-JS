2527c276d12ba4b95e680573b1afcbddf74903a2
antonypauson <13antonysun@gmail.com>
2025-05-30 08:47:11 +0530

    group/set

---
+++ chapter-6/exercises/group.js
@@ -0,0 +1,45 @@
+class Group {
+    constructor() {
+        this.members = []; 
+    }
+
+    add(value) {
+        if (!this.members.includes(value)) {
+            this.members.push(value); 
+        }
+    }
+
+    delete(value) {
+        if (this.members.includes(value)) {
+            this.members = this.members.filter(each => 
+                each != value
+            )
+        }
+    }
+
+    has(value) {
+        if (this.members.indexOf(value) >= 0) return true;
+        return false; 
+    }
+    
+    static from(array) {
+        let myArray = [...array];
+        let group = new Group()
+
+        for (let each of myArray) {
+            group.add(each);
+        } 
+        return group; 
+    }
+
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+console.log(group.has(30));
+
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+
+
