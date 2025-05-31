2d703dba8bf51bac2cd25ff03833055dcdba503f
antonypauson <13antonysun@gmail.com>
2025-05-31 18:55:12 +0530

    exceptions 2

--- chapter-8/exceptions.js
+++ chapter-8/exceptions.js
@@ -1,15 +1,15 @@
 const prompt = require('prompt-sync')();
 
-// console.log(promptDirection("Which way"));
+//ERROR HANDLING
 
-function promptDirection(question) {
+function promptDirection(question) { //this throws error
     let result = prompt(question);
     if (result.toLowerCase() == "left") return "L";
     if (result.toLowerCase() == "right") return "R";
-    throw new Error(`Invalid direction: ${result}`);
+    throw new Error(`Invalid direction: ${result}`); //error returns
 }
 
-function look() {
+function look() { //this function doesn't concern about error
     if (promptDirection("Which way?") == "L") {
         return "a house";
     } else {
