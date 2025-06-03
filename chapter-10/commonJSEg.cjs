bb18d439f4f46d72a2bb12fb761e3b4d8d33c19b
antonypauson <13antonysun@gmail.com>
2025-06-03 23:02:18 +0530

    ordinal, date-name packages

---
+++ chapter-10/commonJSEg.cjs
@@ -0,0 +1,21 @@
+//imported using CommonJS
+//ES6 is actually prefferred
+const ordinal = require("ordinal");
+const {days, months} = require("date-names");
+
+// console.log(ordinal(50));
+// console.log(days[3]);
+// console.log(months[11]);
+
+console.log(formatDate(new Date(2017, 9, 13), "dddd the Do"));
+
+function formatDate(date, format) {
+    return format.replace(/YYYY|M(MMM)?|Do?|dddd/g, tag => {
+        if (tag == "YYYY") return date.getFullYear();
+        if (tag == "M") return date.getMonth();
+        if (tag == "MMMM") return months[date.getMonth()]
+        if (tag == "D") return date.getDate();
+        if (tag == "Do") return ordinal(date.getDate());
+        if (tag == "dddd") return days[date.getDay()];
+    });
+}
