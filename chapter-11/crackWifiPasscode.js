296c25f54af64a4f0e91316955f984aae40e29d8
antonypauson <13antonysun@gmail.com>
2025-06-04 23:31:48 +0530

    cracking password with promise

--- chapter-11/crackWifiPasscode.js
+++ chapter-11/crackWifiPasscode.js
@@ -5,3 +5,22 @@ function withTimeout(promise, time) {
     })
 }
 
+function crackPasscode(networkID) {
+  function nextDigit(code, digit) {
+    let newCode = code + digit;
+    return withTimeout(joinWifi(networkID, newCode), 50)
+      .then(() => newCode)
+      .catch(failure => {
+        if (failure == "Timed out") {
+          return nextDigit(newCode, 0);
+        } else if (digit < 9) {
+          return nextDigit(code, digit + 1);
+        } else {
+          throw failure;
+        }
+      });
+  }
+  return nextDigit("", 0);
+}
+
+crackPasscode("HANGAR 2").then(console.log);
