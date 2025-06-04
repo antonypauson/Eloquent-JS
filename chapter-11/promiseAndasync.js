2401d53db0fe2f163863eb79b836c31654a1bb63
antonypauson <13antonysun@gmail.com>
2025-06-05 00:06:59 +0530

    async await

--- chapter-11/promiseAndasync.js
+++ chapter-11/promiseAndasync.js
@@ -10,15 +10,22 @@ function getExampleText() {
   })
 }
 
+//async function aaki ithine
 async function getExampleText() {
-    const res = await fetch('http://example.com')
-    const text = await res.text(); 
+    const res = await fetch('http://example.com') //await instead of then()
+    const text = await res.text(); //await instead of then()
     console.log("text of example.com", text);
     return text; 
 
 }
 
+//use .then() or await to call 
 getExampleText().then(result => {
     console.log(result); 
 })
+
+async function anotherFunction(params) {
+    const text = await getExampleText();
+    //do something with this text
+}
 
