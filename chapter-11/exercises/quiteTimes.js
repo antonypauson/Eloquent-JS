e0c796f72b182434941abe1d316c44451e79001e
antonypauson <13antonysun@gmail.com>
2025-06-05 22:39:42 +0530

    comments on quiteTimes

--- chapter-11/exercises/quiteTimes.js
+++ chapter-11/exercises/quiteTimes.js
@@ -1,17 +1,17 @@
 async function activityTable(day) {
   let table = [];
-  for (let i = 0; i < 24; i++) table[i] = 0; 
+  for (let i = 0; i < 24; i++) table[i] = 0;  //rep 24 hours
 
-  let logFileList = await textFile("camera_logs.txt");
+  let logFileList = await textFile("camera_logs.txt"); //read first file
   
-  for (let filename of logFileList.split("\n")) {
-    let log = await textFile(filename);
+  for (let filename of logFileList.split("\n")) { //loop through that 
+    let log = await textFile(filename); //read each of that
     
-    for (let timestamp of log.split("\n")) {
+    for (let timestamp of log.split("\n")) { //loop through that
       
-      let date = new Date(Number(timestamp));
+      let date = new Date(Number(timestamp));//check if the day is matching our day
       if (date.getDay() == day) {
-        table[date.getHours()]++; 
+        table[date.getHours()]++;  //get hours and increment it
       }
     }
   }
