48c6dbcf827dc0b6242d20c525e229adae61aa01
antonypauson <13antonysun@gmail.com>
2025-06-05 21:02:33 +0530

    file size join

--- chapter-11/fileSize.js
+++ chapter-11/fileSize.js
@@ -7,3 +7,30 @@ async function fileSizes(files) {
     return list; 
 }
 
+async function fileSize(files) {
+    let lines = files.map(async fileName => {
+        return fileName + ": " + 
+        (await textFile(fileName)).length; 
+    });
+    return (await Promise.all(lines).join("\n"));
+}
+
+
+let list = "";
+
+await Promise.all([
+    (async () => { //async function
+        await wait(100); //wait for 100
+        list += "A\n"; //then only this
+    })(), 
+    (async () => {
+        await wait(50); 
+        list += "B\n"; 
+    })()
+]); 
+
+function wait(time) {
+    return new Promise(accept => setTimeout(accept, time)); 
+}
+
+console.log(list);
