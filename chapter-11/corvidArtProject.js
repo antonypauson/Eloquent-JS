c3e57eb096c7c67dec784463e30d687722d3dd80
antonypauson <13antonysun@gmail.com>
2025-06-05 12:12:01 +0530

    display frame

--- chapter-11/corvidArtProject.js
+++ chapter-11/corvidArtProject.js
@@ -8,3 +8,17 @@ for (let addr = 1; addr < 256; addr++) {
     .then(() => console.log(`Request to ${ip} accepted`))
     .catch(() => {}); 
 }
+
+const screenAddresses = [
+  "10.0.0.44", "10.0.0.45", "10.0.0.41",
+  "10.0.0.31", "10.0.0.40", "10.0.0.42",
+  "10.0.0.48", "10.0.0.47", "10.0.0.46"
+];
+
+function displayFrame(frame) {
+    return Promise.all(frame.map((data, i) => {
+        return request(screenAddresses[i], 
+            {command: "display", data}
+        )
+    }))
+}
