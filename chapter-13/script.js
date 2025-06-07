d03d62afc8877b5d3615f5f8aed3c9286ccf7b84
antonypauson <13antonysun@gmail.com>
2025-06-07 23:29:55 +0530

    iterating through nodes

--- chapter-13/script.js
+++ chapter-13/script.js
@@ -1,3 +1,26 @@
 let body = document.body;
 
-console.log(body.firstChild.parentNode);
+// console.log(body.childNodes[0]);
+
+// console.log(body.children[0]);
+// console.log(body.children[1]);
+// console.log(body.children[2]);
+// console.log("*******")
+
+function talksAbout(node, string) {
+    if (node.nodeType == Node.ELEMENT_NODE) {
+        for (let child of node.childNodes) {
+            if (talksAbout(child, string)) {
+                return true; 
+            }
+        }
+        return false; 
+    } else if (node.nodeType == Node.TEXT_NODE) {
+        return node.nodeValue.indexOf(string) > -1; 
+    }
+}
+
+console.log(talksAbout(body, "book"));
+
+
+console.log(body.childNodes[5].childNodes[0].nodeValue)
