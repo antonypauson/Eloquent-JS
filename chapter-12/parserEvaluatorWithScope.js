9c96b6ee0a805485e534474f6276d265e5128c8c
antonypauson <13antonysun@gmail.com>
2025-06-07 14:38:07 +0530

    evaluator

--- chapter-12/parserEvaluatorWithScope.js
+++ chapter-12/parserEvaluatorWithScope.js
@@ -4,18 +4,36 @@ function evaluate(expression, scope) {
     if (expression.type == "value") {
         return expression.value; 
     } else if (expression.type == "word") {
-        return scope[expression.name]; 
-    } else {
-        throw new ReferenceError(`Undefined binding: ${expression.name}`); 
+        if (expression.name in scope) {
+            return scope[expression.name]; 
+        } else {
+            throw new ReferenceError(`Undefined binding: ${expression.name}`); 
+        }
+    } else if (expression.type == "apply") {
+        let {operator, args} = expression;
+        if (operator.type == "word" && operator.name in specialForms) {
+            return specialForms[operator.name](args, scope); 
+        } else {
+            let op = evaluate(operator, scope); 
+            if (typeof op == "function") {
+                return op(...args.map(arg => evaluate(arg, scope))); 
+            } else {
+                throw new TypeError("Applying a nonfunction"); 
+            }
+        }
     }
+
 }
 
 let myExpression = {
-    type: 'word', 
-    name: 'a'
+  type: 'apply',
+  operator: { type: 'word', name: '+' },
+  args: [ { type: 'word', name: 'a' }, { type: 'value', value: '10' } ]
 }
 
+
 let myScope = {
-    b: 20
+    a: 10, 
+    '+': (x,y) => x + y
 }
 console.log(evaluate(myExpression, myScope)); 
