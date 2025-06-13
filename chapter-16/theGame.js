bd66545d9201e940f1ce3705f3556b0c5398c88b
antonypauson <13antonysun@gmail.com>
2025-06-13 22:06:16 +0530

    elt

--- chapter-16/theGame.js
+++ chapter-16/theGame.js
@@ -141,4 +141,18 @@ const levelChars = {
     "|": Lava,
     "v": Lava
 }
-// const level = new Level(simpleLevelPlan); 
+const level = new Level(simpleLevelPlan);
+console.log(`${level.width} by ${level.height}`);  
+
+function elt(name, attrs, ...children) {
+    let dom = document.createElement(name);
+    for (let attr of Object.keys(attrs)) {
+        dom.setAttribute(attr, attrs[attr]); 
+    }
+
+    for (let child of children) {
+        dom.appendChild(child); 
+    }
+    return dom; 
+}
+
