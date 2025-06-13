5c72dd48d184b4c2f7a1ac182a3686628632ed60
antonypauson <13antonysun@gmail.com>
2025-06-13 19:12:41 +0530

    player

--- chapter-16/theGame.js
+++ chapter-16/theGame.js
@@ -80,8 +80,20 @@ class Coin {
 }
 
 class Player {
+    constructor(pos, speed) {
+        this.pos = pos; 
+        this.speed = speed; 
+    }
+    
+    get type() {
+        return "player"; 
+    }
 
+    static create(pos) { //initial position and initial speed
+        return new Player(pos.plus(new Vec(0, -0.5)), new Vec(0,0)); 
+    }
 }
 
+Player.prototype.size = new Vec(0.8, 1.5); 
 
 const level = new Level(simpleLevelPlan); 
