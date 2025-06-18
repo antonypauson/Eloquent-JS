4bcaf063bb409f7cfd3f4eb547dfa35c34a09921
antonypauson <13antonysun@gmail.com>
2025-06-18 13:56:53 +0530

    diff inputs

--- chapter-18/fetch.js
+++ chapter-18/fetch.js
@@ -2,10 +2,10 @@
 // .then(response => response.text())
 // .then(text => console.log(text));
 
-// fetch("https://en.wikipedia.org/wiki/Denmark", {method: "DELETE"})
-// .then(response => {
-//     console.log(response.status);
-// })
+fetch("https://en.wikipedia.org/wiki/Denmark", {headers: {Range: "bytes=8-19"}})
+.then(response => {
+    console.log(response.status);
+})
 
-fetch("https://pages.github.com/", {method:"POST"})
-.then(response => console.log(response.status));
+// fetch("https://pages.github.com/", {method:"POST"})
+// .then(response => console.log(response.status));
