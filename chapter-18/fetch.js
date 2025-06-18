a7b706934620106218f6b8ce0d8a5b27245cd3de
antonypauson <13antonysun@gmail.com>
2025-06-18 13:31:14 +0530

    fetch method

--- chapter-18/fetch.js
+++ chapter-18/fetch.js
@@ -1,6 +1,11 @@
-fetch("https://en.wikipedia.org/wiki/Denmark").then(
-    response => {
-        console.log(response.status);
-        console.log(response.headers.get("Content-Type"));
-    }
-)
+// fetch("https://en.wikipedia.org/wiki/Denmark")
+// .then(response => response.text())
+// .then(text => console.log(text));
+
+// fetch("https://en.wikipedia.org/wiki/Denmark", {method: "DELETE"})
+// .then(response => {
+//     console.log(response.status);
+// })
+
+fetch("https://pages.github.com/", {method:"POST"})
+.then(response => console.log(response.status));
